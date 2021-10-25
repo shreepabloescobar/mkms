e95fa408a7f4db79d3b7d19e836f2f85ceb8d21b
Nandy <nandhirajanece@gmail.com>
2021-10-25 23:50:16 +0530

    Adding SF api with tyrion plugins

---
+++ server/src/util.js
@@ -0,0 +1,33 @@
+const { find } = require("lodash");
+
+const { Salesforce } = require("@byjus-orders/tyrion-plugins");
+const { ByjusConfig } = require("@byjus-orders/nexemplum/oms");
+
+const setupSFToken = async () => {
+    const SalesforceConfig = await ByjusConfig.findOne({
+        formattedAppName: "SALESFORCE",
+        formattedModuleName: "SALESFORCE_API_CONFIG",
+    });
+
+    if (SalesforceConfig) {
+        const { configs = [] } = SalesforceConfig;
+        const config = find(configs, { environment: process.env.NODE_ENV, orgName: process.env.BYJUS_ORG_NAME });
+        const { authUrl, userName, password, clientId, clientSecret } = config;
+
+        const sfClient = new Salesforce({
+            authUrl,
+            userName,
+            password,
+            clientId,
+            clientSecret,
+        });
+        await sfClient.init();
+        return sfClient;
+    }
+
+    throw new Error(`Salesforce configuration is missing`);
+};
+
+module.exports = {
+    setupSFToken
+};
